0d038c83f80c8927ca9b9d3877d2064a104582a4
fwj199506-sudo <fwj199506@gmail.com>
2026-02-27 17:29:38 +0800

    Add files via upload

--- config.py
+++ config.py
@@ -1,10 +1,9 @@
 # config.py
 import os
 import tushare as ts
-import streamlit as st
 
 # 1. 基础配置
-TS_TOKEN = st.secrets.get("TS_TOKEN", "如果本地没配置这里可以填你的Token做备用")
+TS_TOKEN = 'c7b414cc0540544c00b7485e4fd011d6509229ad846b38e48ed2d401'
 
 # 2. 网络修复 (解决你之前的 Read Timeout 问题)
 os.environ['http_proxy'] = ''
@@ -17,6 +16,4 @@ pro = ts.pro_api(timeout=60)
 # 4. 全局业务常数
 VOL_DECAY = 0.06      # EWMA 衰减因子 (1 - 0.94)
 ANNUAL_DAYS = 252     # 年化交易日
-
 DEFAULT_RF = 0.025    # 默认无风险利率
-
